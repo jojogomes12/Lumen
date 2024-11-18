dd90d2e319ad472d37b14831c9e15d966d8a5c47
jojogomes12 <joaoecarol39@gmail.com>
2024-11-18 01:10:01 -0300

    First Commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => password_hash($request->input('password'), PASSWORD_BCRYPT)
+        ]);
+
+        return response()->json(['message' => 'Usuário criado com sucesso!', 'user' => $user], 201);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+$router->get('/test',function () {
+return "Olá , este é o lumnen!";
+});
+
+//$router->get('/test2','UserController@showMessage');
+$router->post('/test3','UserController@store');
